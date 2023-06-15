dd8b8b73b7d5b10818311fe69608b691d42925e4
BartoszZygmunt <it@bartoszzygmunt.pl>
2023-06-16 01:27:42 +0200

    gal1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
 // Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const galleryElement = document.querySelector('.gallery');
+
+//tworzę znaczniki html
+const galleryItemsMarkup = galleryItems.map(item => {
+  return `
+    <li>
+      <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+      </a>
+    </li>
+  `;
+});
+
+//dodaję do DOM
+// galleryElement.innerHTML = galleryItemsMarkup.join("");
+galleryElement.insertAdjacentHTML('beforeend', galleryItemsMarkup.join(''));
+
+//inicjuję galerię
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
