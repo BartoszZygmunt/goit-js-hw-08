e7df21f147698115c1913e9ddfda7934e14100b7
BartoszZygmunt <it@bartoszzygmunt.pl>
2023-06-16 11:57:21 +0200

    `update`

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,24 @@
 import Player from '@vimeo/player';
+var _ = require('lodash');
+//import throttle from 'lodash/throttle';
 
 const player = new Player('vimeo-player', {});
-
-//usuń nasłuchiwanie przy stop
-player.on('pause', () => {
-  player.off('timeupdate');
-});
 
 const timeStorage = localStorage.getItem('videoplayer-current-time');
 if (timeStorage !== null) {
   player.setCurrentTime(timeStorage);
 }
 
-player.on('timeupdate', () => {
+const setTimeToStorage = () => {
   player
     .getCurrentTime()
     .then(function (seconds) {
       console.log(seconds);
       localStorage.setItem('videoplayer-current-time', seconds);
-      // seconds = the current playback position
     })
     .catch(function (error) {
       console.log(error);
     });
-});
+};
+
+player.on('timeupdate', _.throttle(setTimeToStorage, 1000));
